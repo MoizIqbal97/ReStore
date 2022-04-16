ac67016fc92c1048c4b6ff117c4abf7ded6aa878
Work <work@macbook-c02fw3tsml85.cust.communityfibre.co.uk>
2022-04-16 18:08:48 +0100

    Added front end

--- API/Startup.cs
+++ API/Startup.cs
@@ -6,22 +6,18 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
-
 namespace API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration) //DI configuration injected into this class
+        public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services) //DI container
+        public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
             services.AddSwaggerGen(c =>
             {
@@ -31,6 +27,7 @@ namespace API
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,10 +39,13 @@ namespace API
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
-
-            //app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
 
             app.UseRouting();
+            app.UseCors(opt => 
+            {
+                opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            });
 
             app.UseAuthorization();
 
---
+++ client/src/app/layout/App.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import { Product } from "../models/product";
+
+
+
+function App() {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/products')
+    .then(response => response.json())
+    .then(data => setProducts(data))
+  },[])
+  const addProduct=()=>{
+    setProducts(prevState => [...prevState,
+      {
+        id: prevState.length +101,
+        name:'product' + ( prevState.length + 1 ),
+        price:(prevState.length * 100),
+        brand: 'Gcse',
+        description: 'some description',
+        pictureUrl: 'http://picsum.photos/200'
+      }]) //spread operater ...
+  }
+
+  return (
+    <div>
+    <h1>Re-Store</h1>
+    <ul>
+      {products.map(product =>(
+        <li key={product.name}>{product.name} - {product.price}</li>
+      ))}
+    </ul>
+    <button onClick={addProduct}>Add product</button>
+    </div>
+  );
+}
+
+export default App;
