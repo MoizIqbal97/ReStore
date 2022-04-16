dd4b3528a9d6d5c4afbd78bc7c0eb24a4a9aad83
Work <work@macbook-c02fw3tsml85.cust.communityfibre.co.uk>
2022-04-16 18:27:49 +0100

    added catalog component

---
+++ client/src/features/Catalog/Catalog.tsx
@@ -0,0 +1,18 @@
+import { Product } from "../../app/models/product"
+
+interface Props {
+    products: Product[];
+    addProduct: () => void;
+}
+export const Catalog = (props: Props) => {
+    return(
+    <>
+        <ul>
+            {props.products.map(product =>(
+                <li key={product.id}>{product.name} - {product.price}</li>
+            ))}
+        </ul>
+        <button onClick={props.addProduct}>Add product</button>
+    </>            
+    )
+}
