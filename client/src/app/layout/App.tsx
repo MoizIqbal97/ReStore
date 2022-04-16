41915405cdf44ef9a4ef44aabb783bb5301ba931
Work <work@macbook-c02fw3tsml85.cust.communityfibre.co.uk>
2022-04-16 18:49:34 +0100

    added material ui and made ui changes

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Catalog } from "../../features/Catalog/Catalog";
 import { Product } from "../models/product";
@@ -25,10 +26,10 @@ function App() {
   }
 
   return (
-    <div>
-    <h1>Re-Store</h1>
+    <>
+    <Typography variant = 'h1'>Re-Store</Typography>
     <Catalog products ={products} addProduct={addProduct}/>
-    </div>
+    </>
   );
 }
 
