1ce80c73fe83e28f77599a26c5482203bfcac710
Work <work@macbook-c02fw3tsml85.cust.communityfibre.co.uk>
2022-04-18 11:37:34 +0100

    Added react router

---
+++ client/src/features/Catalog/ProductDetails.tsx
@@ -0,0 +1,9 @@
+import { Typography } from "@mui/material"
+
+export const ProductDetails=()=>{
+    return (
+        <Typography variant='h2'>
+            Product detail        
+        </Typography>
+    )
+}
